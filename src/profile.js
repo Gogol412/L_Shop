02d485a105e7e103a770906fa381f349f18111de
Misha <micha990000@gmail.com>
2026-03-12 14:57:54 +0300

    end up profile page

--- src/profile.js
+++ src/profile.js
@@ -1,36 +1,60 @@
 import React from 'react';
 import "./style.css";
 import { Link } from 'react-router-dom';
+import cupcake from "./Капкейки.jpg";
+import cheescake from "./Чизкейки.jpg";
 
 const Profile = () => {
+  const profileProducts = [
+    { id: 1, name: "Капкейки", price: "50 BYN/10 шт.", image: cupcake },
+    { id: 2, name: "Чизкейки", price: "55 BYN/8 шт.", image: cheescake },
+    { id: 3, name: "Капкейки", price: "50 BYN/10 шт.", image: cupcake },
+    { id: 4, name: "Чизкейки", price: "55 BYN/8 шт.", image: cheescake },
+  ];
+
   return (
     <>
-    <div className="Catalog">
+      <div className="Catalog">
         <Link to="/catalog" style={{ textDecoration: 'none', color: 'inherit' }}>
-            <p>Каталог</p>
+          <p>Каталог</p>
         </Link>
-    </div>
+      </div>
 
-    <div className='CakesP'>
+      <div className='CakesP'>
         <p>Cakes</p>
-    </div>
+      </div>
 
-    <div className='ProfileBlock'>
+      <div className='ProfileBlock'>
         <p>Имя Фамилия</p>
         <span>+375 (00) 000-00-00</span>
-    </div>
+      </div>
 
-    <div className='DiscountBlock'>
+      <div className='DiscountBlock'>
         <p>Скидка</p>
         <span>До 50%</span>
-    </div>
+      </div>
 
-    <div className='SumOfDeserts'>
+      <div className='SumOfDeserts'>
         <p>Сумма вашей корзины</p>
         <span>1200 BYN</span>
-    </div>
+      </div>
 
+      <div className='YourBuyings'>
+        <p>Ваши покупки</p>
+      </div>
 
+      <div className="profile-grid">
+        {profileProducts.map((product) => (
+          <div key={product.id} className="product-card">
+            <div className="product-image">
+              <img src={product.image} alt={product.name} />
+            </div>
+            <div className="product-name">{product.name}</div>
+            <div className="product-price">{product.price}</div>
+            <div className="product-button">В корзину</div>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
